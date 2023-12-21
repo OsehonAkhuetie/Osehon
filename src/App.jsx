cee0d44546dbd19a2589d3098cd1e89c209f02f0
OsehonAkhuetie <111537526+OsehonAkhuetie@users.noreply.github.com>
2023-12-21 12:28:14 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Osehon Akhuetie",
+  title: "Data Scientist/Machine Learning Engineer",
+  email: "osehon.akhuetie@studentambassadors.com",
+  gitHub: "OsehonAkhuetie",
+  instagram: "osehon_akhuetie",
+  linkedIn: "Osehon_Akhuetie",
   medium: "",
-  twitter: "microsoft",
+  twitter: "theNeuralNinja",
   youTube: "Code",
 };
 
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bluebackground.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Light blue background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at University of Benin. I enjoy creating machine learning models that solve real world problems.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python Programming",
+  "Data Science",
+  "Machine Learning",
+  "Problem solving skills",
+  "Innovative skills",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Computer Science background I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
